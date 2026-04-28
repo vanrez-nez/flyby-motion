c4a8fcaf212142cf5c4af935444df01d3d505d24
Ivan Juarez <ivan@blindgazer.com>
2026-04-28 17:46:54 -0600

    feat: refactor landing page layout to a grid system with a persistent sidebar and reorganized hero section

--- demos/LandingPage.tsx
+++ demos/LandingPage.tsx
@@ -12,24 +12,33 @@ export const LandingPage: React.FC = () => {
 
   return (
     <div className="landing">
-      <img src={logoUrl} alt="Flyby Library" className="landing__logo" style={{ height: '80px', marginBottom: '2rem' }} />
-      <p className="landing__subtitle">Choose a demo to explore:</p>
-      <div className="landing__buttons">
-        <a href="/2d/forces" className="landing__buttons-btn landing__buttons-btn--2d">
-          2D Demo
-        </a>
-        <a href="/3d/forces" className="landing__buttons-btn landing__buttons-btn--3d">
-          3D Demo
-        </a>
+      <div className="landing__grid">
+        <header className="landing__hero">
+          <div className="landing__hero-brand">
+            <img src={logoUrl} alt="Flyby Library" className="landing__logo" />
+          </div>
+          <div className="landing__buttons">
+            <a href="/2d/forces" className="landing__buttons-btn landing__buttons-btn--2d">
+              2D Demo
+            </a>
+            <a href="/3d/forces" className="landing__buttons-btn landing__buttons-btn--3d">
+              3D Demo
+            </a>
+          </div>
+        </header>
+
+        <aside className="landing__sidebar">
+          <MarkdownNav sections={sections} />
+        </aside>
+
+        <main className="landing__main">
+          <article
+            className="markdown-body"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </main>
       </div>
-      <div className="landing__tutorial">
-        <MarkdownNav sections={sections} />
-        <article
-          className="markdown-body"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
-      
+
       <footer className="landing__footer">
         <DemoFooter />
       </footer>
